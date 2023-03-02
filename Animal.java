22f7c3b6ff7b71aa60442765a7d39e886f50e08d
VicktzZ <VictorHugo220Br@gmail.com>
2023-03-02 17:40:15 +0000

    Animal class

--- Animal.java
+++ Animal.java
@@ -1,3 +1,30 @@
 public class Animal {
-    
+    private String nome;
+    private String raca;
+    private double peso;
+
+    public Animal(String nome, String raca, double peso) {
+        this.nome = nome;
+        this.raca = raca;
+        this.peso = peso;
+    }
+
+    public void dormir()
+    {
+        System.out.println("Estou dormindo...");
+    }
+
+    public void comer(String comida)
+    {
+        System.out.println("Estou comendo... " + comida + "!" );
+    }
+
+    public void fazerBarulho(String barulho)
+    {
+        System.out.println("Barulho: " + barulho);
+    }
+
+    public void printar() {
+        System.out.println(nome);
+    }
 }
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal cachorro = new Animal("Vitor", "A", 50.0);
+
+        cachorro.printar();
+    }
+}
